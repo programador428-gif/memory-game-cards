8c06f385fcbd001043253766b69b4f704f969498
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-02-18 11:32:07 -0500

    Funcion del contador, hecho.
    Un contador sencillo, agregado al proyecto, tambien añadi unos id a las imagenes para que funcione de mejor manera.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 const reset = document.getElementById("reset");
+const pantalla = document.querySelector(".game");
 let cartas = document.querySelectorAll(".cartas");
 let lista = Array.from(cartas);
 let i = lista.length,
   j,
   temporal;
 const game = document.querySelector(".game");
+let puntaje = 0;
+let eleccion = [];
+let contador = document.getElementById("contador");
+contador = parseInt(contador.textContent);
+
 // Funciones
+
+// Funcion de reseteo
 reset.addEventListener("click", () => {
   while (--i > 0) {
     j = Math.floor(Math.random() * (i + 1));
@@ -16,9 +24,33 @@ reset.addEventListener("click", () => {
   lista.forEach((elemento) => {
     game.appendChild(elemento);
   });
-  cartas = 0;
+  document.getElementById("contador").textContent = 0;
+  contador = 0;
   cartas = document.querySelectorAll(".cartas");
-  lista = 0;
   lista = Array.from(cartas);
   ((i = lista.length), j, temporal);
+  eleccion = [];
+});
+
+// Funcion del contador
+pantalla.addEventListener("click", (e) => {
+  const carta = e.target.closest(".cartas");
+  eleccion.push(carta);
+  if (eleccion.length === 2) {
+    const [c1, c2] = eleccion;
+    if (
+      c1.querySelector("img").src === c2.querySelector("img").src &&
+      c1.querySelector("img").id !== c2.querySelector("img").id
+    ) {
+      contador = contador + 1;
+      document.getElementById("contador").textContent = contador;
+      eleccion = [];
+    } else {
+      eleccion = [];
+    }
+    // Esto es por si falla algo y se traba pasandose de muchos valores, reinicie
+    if (eleccion.length > 2) {
+      eleccion = [];
+    }
+  }
 });
